5f040a2573b9e1c611ee0b2fb6046d8ff48fdc37
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2017-03-26 00:52:24 +0300

    Add an endpoint to save routes to the database.

--- application/src/routes/route/index.js
+++ application/src/routes/route/index.js
@@ -15,4 +15,18 @@ module.exports = (app) => {
       .then((result) => res.json(result))
       .catch(next);
   });
+
+  app.post('/routes', (req, res, next) => {
+    if(!req.user) next(new Error("This requires an authenticated user!"));
+    const { start, end, routeBody } = req.body;
+    const route = new Route();
+    route.period.start = start;
+    route.period.end = end;
+    route.period.route = routeBody;
+    route.person = req.user._id;
+
+    return route.save()
+      .then((result) => res.json(result))
+      .catch(next);
+  });
 };
