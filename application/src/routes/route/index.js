ee133795ef6a3ef16eb18dcbce59596955525e5b
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2017-03-25 20:22:30 +0300

    Check route result in endpoint, Fix indent.

--- application/src/routes/route/index.js
+++ application/src/routes/route/index.js
@@ -4,16 +4,14 @@ module.exports = (app) => {
   app.get('/routes/:id', (req, res, next) => {
     const { id } = req.params;
     return Route.findById(id)
-      .then((result) => {
-      res.json(result)
-    })
+      .then((result) => res.json(result))
       .catch(next);
   });
 
   app.get('/routes/:id/match', (req, res, next) => {
     const { id } = req.params;
     Route.findById(id)
-      .then(route => Route.findMatch(route))
+      .then(route => route ? Route.findMatch(route) : Promise.reject(route))
       .then((result) => res.json(result))
       .catch(next);
   });
