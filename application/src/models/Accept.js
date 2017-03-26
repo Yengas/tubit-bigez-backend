35b9754dd88a8803fcce70a20782e40747de41b6
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2017-03-26 09:25:00 +0300

    Add a static method to retrieve all accepted routes for a user.

--- application/src/models/Accept.js
+++ application/src/models/Accept.js
@@ -13,5 +13,20 @@ const acceptSchema = new mongoose.Schema({
 // Make sure there is only 1 mongoose record for person/route pairs.
 acceptSchema.index({ person: 1, route: 1 }, { unique: true });
 
+/**
+ * Returns all of the accepted routes ids for a given user_id.
+ * @param id {String} the id of the user to make the query for.
+ * @return {Promise.<TResult>|Promise}
+ */
+acceptSchema.statics.routesForUser = function(id){
+  return this.find({ person: id })
+    .exec()
+    .then(results => {
+      if(!results || results.length == 0) return Promise.reject();
+      return Promise.resolve(results.map(r => r._id));
+    })
+    .catch(() => Promise.resolve([]))
+};
+
 const Accept = mongoose.model('Accept', acceptSchema);
 module.exports = Accept;
